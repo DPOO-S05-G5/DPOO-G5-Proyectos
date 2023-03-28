f6b571e8fb210b7462a3a24805fcc58533810469
giulianavolpi <g.volpi@uniandes.edu.co>
2023-03-28 17:10:38 -0500

    Servicios

--- Proyecto1/Entrega 3/Proyecto1_PMS/src/modelo/Servicio.java
+++ Proyecto1/Entrega 3/Proyecto1_PMS/src/modelo/Servicio.java
@@ -1,5 +1,82 @@
 package modelo;
 
-public class Servicio {
+public abstract class Servicio {
+	
+	private boolean deSpa;
+	private boolean deGuiaTuristico;
+	private String nombre;
+	private int precio;
+	private String ID;
+	public boolean isDeSpa() {
+		return deSpa;
+	}
 
+	public void setDeSpa(boolean deSpa) {
+		this.deSpa = deSpa;
+	}
+
+	public boolean isDeGuiaTuristico() {
+		return deGuiaTuristico;
+	}
+
+	public void setDeGuiaTuristico(boolean deGuiaTuristico) {
+		this.deGuiaTuristico = deGuiaTuristico;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public boolean isDisponibilidad() {
+		return disponibilidad;
+	}
+
+	public void setDisponibilidad(boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	private boolean disponibilidad;
+	private int duracion;
+	
+	
+	public Servicio(boolean deSpa, boolean deGuiaTuristico, String nombre, int precio, String ID, boolean disponibilidad,int duracion )
+	
+	{
+		super();
+		this.deSpa = deSpa;
+		this.deGuiaTuristico = deGuiaTuristico;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.ID = ID;
+		this.disponibilidad = disponibilidad;
+		this.duracion = duracion;
+	}
 }

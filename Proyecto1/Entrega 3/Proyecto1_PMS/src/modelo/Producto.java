84214ff77b7921459da1d20042164d536246e249
giulianavolpi <g.volpi@uniandes.edu.co>
2023-03-28 16:30:39 -0500

    Producto terminado

--- Proyecto1/Entrega 3/Proyecto1_PMS/src/modelo/Producto.java
+++ Proyecto1/Entrega 3/Proyecto1_PMS/src/modelo/Producto.java
@@ -1,5 +1,79 @@
 package modelo;
 
-public class Producto {
+import java.util.ArrayList;
 
+public abstract class Producto {
+	
+	private String nombre;
+	private String ID;
+	private String precio;
+	private boolean servicioComedor;
+	private boolean servicioHabitacion;
+	
+	
+	public Producto(String nombre, String ID, String precio,boolean servicioComedor,boolean servicioHabitacion)
+	{
+		
+	super();
+	this.nombre = nombre;
+	this.ID = ID;
+	this.precio = precio;
+	this.servicioComedor = servicioComedor;
+	this.servicioHabitacion = servicioHabitacion;
+	
+	
+	
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getID() {
+		return ID;
+	}
+
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+
+	public String getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+
+	public boolean isServicioComedor() {
+		return servicioComedor;
+	}
+
+
+	public void setServicioComedor(boolean servicioComedor) {
+		this.servicioComedor = servicioComedor;
+	}
+
+
+	public boolean isServicioHabitacion() {
+		return servicioHabitacion;
+	}
+
+
+	public void setServicioHabitacion(boolean servicioHabitacion) {
+		this.servicioHabitacion = servicioHabitacion;
+	}
+	
+	
 }

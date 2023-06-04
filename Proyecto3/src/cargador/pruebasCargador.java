3e4318377c95afebe350f7b60a9cd5414ea8a17c
giulianavolpi <g.volpi@uniandes.edu.co>
2023-06-04 15:02:58 -0500

    Inicio Pruebas

---
+++ Proyecto3/src/cargador/pruebasCargador.java
@@ -0,0 +1,14 @@
+package Proyecto3.src.cargador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class pruebasCargador {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Proyecto3/src/modelo/ReservaTest.java
@@ -0,0 +1,14 @@
+package Proyecto3.src.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReservaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
